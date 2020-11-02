3c0d3c35bd7d1d2c6b13143fefd76a9b0b2df07f
sothasil <nikolovskialexxandar@gmail.com>
2020-11-02 22:52:24 +0100

    Initial commit basic functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,63 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export default class Geocoder extends Component {
+  state = {
+    results: [],
+    inputValue: '',
+  }
+
+  onChange = (event) => {
+    this.setState({ inputValue: event.target.value });
+    setTimeout(() => {
+      if (this.state.inputValue !== '') {
+        const url = `http://api.geonames.org/search?q=${this.state.inputValue}&type=json&username=${this.props.username}&maxRows=10`;
+        fetch(url)
+          .then((response) => {
+            response.json().then((data) => {
+              this.setState({ results: data.geonames });
+            });
+          });
+      } else {
+        this.setState({ results: [] });
+      }
+    }, this.props.timeout);
+  }
+
+  onSelect = (place) => {
+    const placeName = place.toponymName.concat(
+      place.countryName ? `, ${place.countryName}` : null,
+    );
+    this.setState({ inputValue: placeName });
+  }
+
+  render() {
+    const results = this.state.results.map((place) => (
+      <li
+        onClick={() => this.onSelect(place)}
+      >
+        {place.toponymName.concat(place.countryName ? `, ${place.countryName}` : null)}
+      </li>
+    ));
+    return (
+      <div>
+        <input
+          value={this.state.inputValue}
+          onChange={this.onChange}
+        />
+        <ul>
+          {results}
+        </ul>
+      </div>
+    );
+  }
 }
+
+Geocoder.propTypes = {
+  timeout: PropTypes.number,
+  username: PropTypes.string.isRequired,
+};
+
+Geocoder.defaultProps = {
+  timeout: 300,
+};
