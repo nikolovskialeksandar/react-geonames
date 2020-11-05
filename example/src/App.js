ce4cd2648634e4c46597d00baddca65740ff0b89
sothasil <nikolovskialexxandar@gmail.com>
2020-11-05 03:28:37 +0100

    Added props and their conversion to URL

--- example/src/App.js
+++ example/src/App.js
@@ -7,12 +7,19 @@ require('dotenv').config();
 
 class App extends Component {
   render() {
+    const queryParams = {
+      type: 'json',
+      maxRows: 10,
+    };
     return (
       <Geocoder
         username={process.env.REACT_APP_GEONAMES_USERNAME}
+        https={true}
+        queryParams={queryParams}
+        placeholder="Search"
       />
     );
   }
 }
 
-export default App
+export default App;
