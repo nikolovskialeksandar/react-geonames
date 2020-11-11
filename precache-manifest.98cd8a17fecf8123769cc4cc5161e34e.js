f280a07453e4b34e4b483e700b39d2b6b15633a9
sothasil <nikolovskialexxandar@gmail.com>
2020-11-11 04:43:45 +0100

    Updates

---
+++ precache-manifest.98cd8a17fecf8123769cc4cc5161e34e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2917ec0715a976863ea65dfa7b4ac007",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ad949bb6a013119bae6f",
+    "url": "./static/css/2.0627e603.chunk.css"
+  },
+  {
+    "revision": "7d2443af36b848ee70e0",
+    "url": "./static/css/main.1e0c98e8.chunk.css"
+  },
+  {
+    "revision": "ad949bb6a013119bae6f",
+    "url": "./static/js/2.208a544f.chunk.js"
+  },
+  {
+    "revision": "cc6f759c72c5f0bd740c23c24edeb33d",
+    "url": "./static/js/2.208a544f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d2443af36b848ee70e0",
+    "url": "./static/js/main.1b8f8446.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.1b8f8446.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4745f36fa0037496d399",
+    "url": "./static/js/runtime-main.d1d15e37.js"
+  }
+]);
